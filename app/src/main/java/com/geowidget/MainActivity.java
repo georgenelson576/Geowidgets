c0e6aef3e86290f59d1e4056d2184e369ed456da
georgenelson576 <georgenelson576@gmail.com>
2026-01-29 16:44:04 +0530

    Initial commit

---
+++ app/src/main/java/com/geowidget/MainActivity.java
@@ -0,0 +1,19 @@
+package com.geowidget;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView tv = new TextView(this);
+        tv.setText("yoo boii");
+        tv.setTextSize(24f);
+
+        setContentView(tv);
+    }
+}
